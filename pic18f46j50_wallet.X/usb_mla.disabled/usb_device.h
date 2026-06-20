dec1ea2ca9130c5e1a42f0ab466838ebabc82c64
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-19 22:04:17 -0300

    Reestructurar pic18f4620_detector

--- pic18f46j50_wallet.X/usb_mla.disabled/usb_device.h
+++
@@ -1,4 +0,0 @@
-#ifndef USB_DEVICE_H
-#define USB_DEVICE_H
-#include "inc/usb_device.h"
-#endif
--- pic18f46j50_wallet.X/usb_mla.disabled/usb_device_config.h
+++
@@ -1,4 +0,0 @@
-#ifndef USB_DEVICE_CONFIG_H
-#define USB_DEVICE_CONFIG_H
-#include "inc/usb_device_config.h"
-#endif
